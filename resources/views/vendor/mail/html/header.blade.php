dbd83c93f63decba2657a02f20c0cdf210d8bb9d
Muhammad Abdul Aziz <mabdulaziz@student.telkomuniversity.ac.id>
2026-05-16 15:23:57 +0700

    betulin format email baru jir

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,12 @@
 @props(['url'])
 <tr>
 <td class="header">
-    <a href="{{ $url }}" style="display: inline-block;">
-        <img src="{{ asset('images/icon-siem-aziz.png') }}" alt="Logo SIEM" style="height: 250px;">
-    </a>
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://i.pinimg.com/1200x/95/e7/75/95e77567dc1be99790ba91975ed5ccc3.jpg" class="logo" alt="Laravel Logo">
+@else
+{!! $slot !!}
+@endif
+</a>
 </td>
 </tr>
