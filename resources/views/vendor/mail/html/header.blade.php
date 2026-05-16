31b0ac94c4b1b061dd6ed36d2bebccd684505538
Muhammad Abdul Aziz <mabdulaziz@student.telkomuniversity.ac.id>
2026-05-16 15:16:20 +0700

    benerin format email

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+    <a href="{{ $url }}" style="display: inline-block;">
+        <img src="{{ asset('images/icon-siem-aziz.png') }}" alt="Logo SIEM" style="height: 250px;">
+    </a>
+</td>
+</tr>
