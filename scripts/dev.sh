5cfead1215cb3e7c724f283c537b9a7dc7bc6832
azizhehe02 <azizhehe01@gmail.com>
2026-09-09 21:21:44 +0700

    membuat composer run juga ikut run mariadb

---
+++ scripts/dev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "Menyalakan MariaDB...🦭🦭🦭🦭🦭🦭🦭🦭🦭"
+sudo systemctl start mariadb
+
+cleanup() {
+    echo
+    echo "Menghentikan MariaDB..."
+    sudo systemctl stop mariadb
+    echo "MariaDB sudah dimatikan."
+}
+
+trap cleanup EXIT INT TERM
+
+echo "Menjalankan Laravel development environment..."
+
+npx concurrently -c "#93c5fd,#c4b5fd,#fdba74" \
+    "php artisan serve" \
+    "php artisan queue:listen --tries=1" \
+    "npm run dev"
